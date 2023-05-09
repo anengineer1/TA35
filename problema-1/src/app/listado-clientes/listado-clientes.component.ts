8be7086f8266432f33ba5db654648717d53779ae
Francisco Parra <franparpe@disroot.org>
2023-05-09 16:29:01 +0200

    First commit

---
+++ problema-1/src/app/listado-clientes/listado-clientes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-clientes',
+  templateUrl: './listado-clientes.component.html',
+  styleUrls: ['./listado-clientes.component.css']
+})
+export class ListadoClientesComponent {
+
+}
